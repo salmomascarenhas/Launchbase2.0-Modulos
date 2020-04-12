0216dd518a5640826dd670933048b61cf2651f35
Salmo <salmo.cruz@gmail.com>
2020-04-12 18:44:17 -0300

    all project finished

---
+++ modulo03/data.js
@@ -0,0 +1,40 @@
+module.exports = [
+    {
+        id: 'SE76RtsU1VE',
+        title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure ea repellendus molestias esse aperiam...',
+        duration: '30 min',
+        price: 'Free',
+        featured: true
+    },
+    {
+        id: 'SE76RtsU1VE',
+        title: 'Jquery morreu ? E agora ?',
+        duration: '30 min',
+        price: 'Free'
+    },
+    {
+        id: 'SE76RtsU1VE',
+        title: 'Jquery morreu ? E agora ?',
+        duration: '30 min',
+        price: 'Free'
+    },
+    {
+        id: 'SE76RtsU1VE',
+        title: 'Jquery morreu ? E agora ?',
+        duration: '30 min',
+        price: 'Free'
+    },
+    {
+        id: 'SE76RtsU1VE',
+        title: 'Jquery morreu ? E agora ?',
+        duration: '30 min',
+        price: 'Free'
+    },
+    {
+        id: 'SE76RtsU1VE',
+        title: 'Jquery morreu ? E agora ?',
+        duration: '30 min',
+        price: 'Free',
+        featured: true
+    }
+];
