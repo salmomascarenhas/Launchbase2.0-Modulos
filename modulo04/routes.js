44bd18a91592d99547cb4333b71a4102487a00bd
Salmo <salmo.cruz@gmail.com>
2020-07-17 15:27:21 -0300

    form validation

--- modulo04/routes.js
+++ modulo04/routes.js
@@ -10,6 +10,15 @@ routes.get('/instructors', function (req, res) {
     return res.render('instructors/index');
 });
 
+routes.get('/instructors/create', function (req, res) {
+    return res.render('instructors/create');
+});
+
+routes.post('/instructors', function (req, res) {
+
+    const keys = Object.keys(req.body) /
+});
+
 routes.get('/members', function (req, res) {
     return res.send('members');
 });
