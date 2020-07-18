103d2d1622848e52b744b5d0fdeabd3d31cf541a
Salmo <salmo.cruz@gmail.com>
2020-07-18 13:41:53 -0300

    a

--- modulo04/routes.js
+++ modulo04/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = express.Router();
+const instructors = require('./instructors');
 
 
 routes.get('/', function (req, res) {
@@ -10,14 +11,12 @@ routes.get('/instructors', function (req, res) {
     return res.render('instructors/index');
 });
 
+routes.post('/instructors', instructors.post);
+
 routes.get('/instructors/create', function (req, res) {
     return res.render('instructors/create');
 });
 
-routes.post('/instructors', function (req, res) {
-
-    const keys = Object.keys(req.body) /
-});
 
 routes.get('/members', function (req, res) {
     return res.send('members');
